4f63d727787f4c2830fbeb8a4ba2b9b76237c302
刘群 <64795+liuqun@users.noreply.github.com>
2023-06-08 14:08:05 +0800

    更换高德自定义地图样式

--- src/views/amapLocaTest/index.vue
+++ src/views/amapLocaTest/index.vue
@@ -2,7 +2,7 @@
   <div class="amapLocaTestView">
     <h2>高德地图Loca测试</h2>
     <!-- eslint-disable-next-line vue/attribute-hyphenation -->
-    <amap-view styleId="dccd1c0abf29ebdf05fed63ff309a804" :zoom="13">
+    <amap-view styleId="6b11188d6c2ef3c4220097ae97dac30e" :zoom="13">
       <amap-loca-point />
     </amap-view>
   </div>
